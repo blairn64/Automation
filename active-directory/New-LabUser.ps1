4ed9ffc38844cde80a3fea610ed8e74c5bfd7b74
t0w3r <blairn64@gmail.com>
2026-08-27 12:25:54 -0400

    Add sanitized Active Directory onboarding example

---
+++ active-directory/New-LabUser.ps1
@@ -0,0 +1,38 @@
+[CmdletBinding(SupportsShouldProcess)]
+param(
+    [Parameter(Mandatory)] [string]$SamAccountName,
+    [Parameter(Mandatory)] [string]$GivenName,
+    [Parameter(Mandatory)] [string]$Surname,
+    [Parameter(Mandatory)] [string]$UserPrincipalName,
+    [string]$Path = 'OU=LabUsers,DC=example,DC=invalid',
+    [string[]]$Groups = @()
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+if (-not (Get-Command New-ADUser -ErrorAction SilentlyContinue)) {
+    throw 'ActiveDirectory PowerShell module is not available.'
+}
+
+$displayName = "$GivenName $Surname"
+
+if ($PSCmdlet.ShouldProcess($displayName, 'Create lab Active Directory user')) {
+    New-ADUser \
+        -SamAccountName $SamAccountName \
+        -UserPrincipalName $UserPrincipalName \
+        -GivenName $GivenName \
+        -Surname $Surname \
+        -Name $displayName \
+        -DisplayName $displayName \
+        -Path $Path \
+        -Enabled $false
+
+    foreach ($group in $Groups) {
+        if ($PSCmdlet.ShouldProcess($group, "Add $displayName to group")) {
+            Add-ADGroupMember -Identity $group -Members $SamAccountName
+        }
+    }
+}
+
+Write-Output "Prepared lab user $displayName ($SamAccountName)"
